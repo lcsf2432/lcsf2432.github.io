a563f199e1630130c1c2f7afc5169d9f1105c341
Divi Barvaliya <c0957873@mylambton.ca>
2024-12-04 22:20:29 -0500

    Added hamburger for mobile nav

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,16 @@
     File Name: script.js
     Date: 1 Dec 2024
 */
+
+// Hamburger menu function
+function hamburger() {
+  var menu = document.getElementById("menu-links");
+  var logo = document.getElementById("tp_logo");
+  if (menu.style.display === "block" && logo.style.display === "none") {
+    menu.style.display = "none";
+    logo.style.display = "block";
+  } else {
+    menu.style.display = "block";
+    logo.style.display = "none";
+  }
+}
